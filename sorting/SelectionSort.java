a02f9c4cc4027d32901f96938265ea2bc59890b8
KJS1124 <34736289+KJS1124@users.noreply.github.com>
2018-01-05 12:25:06 +0530

    Update SelectionSort.java

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -4,51 +4,43 @@ import java.util.Arrays;
 
 public class SelectionSort {
 
-	public int[] selectionSortInIncreasing(int arr[])
-	{
-		for(int i=0;i<arr.length;i++)
-		{
-			int index =i;
-			for(int j =i ; j<arr.length;j++)
-			{
-				if(arr[index] > arr[j]) {
-				index = j;
+	public int[] selectionSortInIncreasing(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+			int index = i;
+			for (int j = i; j < arr.length; j++) {
+				if (arr[index] > arr[j]) {
+					index = j;
 				}
 			}
 			int temp = arr[i];
-			arr[i]=arr[index];
-			arr[index]=temp;
+			arr[i] = arr[index];
+			arr[index] = temp;
 		}
 		return arr;
 	}
-	
-	
-	public int[] selectionSortInDecreasing(int arr[])
-	{
-		for(int i=0;i<arr.length;i++)
-		{
-			
-			int index =i;
-			for(int j =i ; j<arr.length;j++)
-			{
-				if(arr[index] < arr[j]) {
-				index = j;
+
+	public int[] selectionSortInDecreasing(int arr[]) {
+		for (int i = 0; i < arr.length; i++) {
+
+			int index = i;
+			for (int j = i; j < arr.length; j++) {
+				if (arr[index] < arr[j]) {
+					index = j;
 				}
 			}
 			int temp = arr[i];
-			arr[i]=arr[index];
-			arr[index]=temp;
+			arr[i] = arr[index];
+			arr[index] = temp;
 		}
 		return arr;
 	}
-	
-	public static void main(String agrs[])
-	{
-		int arr[]= {14,56,2,7,9,2,1,5,80,7,5,432};
-		int arr2[]= {14,56,2,7,9,2,1,5,80,7,5,432};
-		SelectionSort ss =new SelectionSort();
-		Arrays.stream(ss.selectionSortInIncreasing(arr)).forEach(n-> System.out.print(n+" "));
+
+	public static void main(String agrs[]) {
+		int arr[] = { 14, 56, 2, 7, 9, 2, 1, 5, 80, 7, 5, 432 };
+		int arr2[] = { 14, 56, 2, 7, 9, 2, 1, 5, 80, 7, 5, 432 };
+		SelectionSort ss = new SelectionSort();
+		Arrays.stream(ss.selectionSortInIncreasing(arr)).forEach(n -> System.out.print(n + " "));
 		System.out.println();
-		Arrays.stream(ss.selectionSortInDecreasing(arr2)).forEach(n-> System.out.print(n+" "));
+		Arrays.stream(ss.selectionSortInDecreasing(arr2)).forEach(n -> System.out.print(n + " "));
 	}
 }
