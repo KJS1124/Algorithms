b8ce435831bc3fa5bb0001bef50497780c2e57a4
KJS1124 <34736289+KJS1124@users.noreply.github.com>
2018-01-11 14:03:17 +0530

    Add files via upload

---
+++ search/MaximumSumSubArray.java
@@ -0,0 +1,30 @@
+package search;
+/**
+ * @author KJS1124
+ * Time Complexity O(n)
+ * Auxiliary Space: O(n)
+ * Problem link http://www.spoj.com/problems/MAXSUMSU/
+ */
+public class MaximumSumSubArray {
+
+	public int maximumSubArraySum(int arr[])
+	{
+		int maximumSumSoFar=0;
+		int maximumSumTillNow=0;
+		for(int i=0 ; i < arr.length;i++)
+		{
+			maximumSumTillNow+=arr[i];
+			if(maximumSumTillNow<0)
+				maximumSumTillNow=0;
+			maximumSumSoFar = Math.max(maximumSumSoFar, maximumSumTillNow);
+		}
+		return maximumSumSoFar;
+	}
+	
+	public static void main(String agrs[])
+	{
+		int arr[] = {2, 3, 4, 5, 7};
+		MaximumSumSubArray maximumSumSubArray = new MaximumSumSubArray();
+		System.out.println(maximumSumSubArray.maximumSubArraySum(arr));
+	}
+}
