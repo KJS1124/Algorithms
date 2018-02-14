2d672a640dd22aeb782c8d41850e1625155fdea9
Karanjot Singh <karanjotsingh1124@gmail.com>
2018-02-14 23:04:58 +0530

    adding modular aithmatic operations

---
+++ math/ModularAirthmatic.java
@@ -0,0 +1,25 @@
+public class ModularAirthmatic {
+
+    int mod = 0;
+
+    public ModularAirthmatic(int mod) {
+        this.mod = mod;
+    }
+
+    public int add(int a, int b) {
+        return (a % mod + b % mod) % mod;
+    }
+
+    public int subtract(int a, int b) {
+        return ((a + mod - b % mod) + mod) mod;
+    }
+
+    public int multiply(int a, int b) {
+        return (a % mod * b % mod) % mod;
+    }
+
+    public int divide(int a, int b) {
+        return (a % mod / inversemod(b)) % mod;
+    }
+
+}
